ff5938510fb9426eaafdd4e28001e3c07de39868
Rubén Gavilán <rgavilan@izertis.com>
2025-08-11 11:30:22 +0200

    Update newfile.py

--- server/newfile.py
+++ server/newfile.py
@@ -2,3 +2,7 @@ import pickle
 
 data = b"cosas serializadas"
 obj = pickle.loads(data)  # Vulnerabilidad: deserialización no segura
+
+
+user_input = "print('hola')"
+eval(user_input)  # Vulnerabilidad: permite ejecución arbitraria
